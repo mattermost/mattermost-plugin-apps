4cf66f1b705ed6efb68859fb09954c0c00372061
Jason Frerich <jason.frerich@mattermost.com>
2020-09-18 14:11:18 -0500

    Implement StoreSubscription and DeleteSubscription

--- server/apps/subscriptions.go
+++ server/apps/subscriptions.go
@@ -4,7 +4,9 @@
 package apps
 
 import (
+	pluginapi "github.com/mattermost/mattermost-plugin-api"
 	"github.com/mattermost/mattermost-plugin-apps/server/configurator"
+	"github.com/pkg/errors"
 )
 
 const SubsPrefixKey = "sub"
@@ -13,12 +15,13 @@ type Subscriptions interface {
 	GetSubscriptionsForChannel(subj SubscriptionSubject, channelID string) ([]*Subscription, error)
 	GetSubscriptionsForTeam(subj SubscriptionSubject, teamID string) ([]*Subscription, error)
 	GetSubscriptionsForApp(appID string, subj SubscriptionSubject, teamID string) ([]*Subscription, error)
-	StoreSubcription(subj SubscriptionSubject, channelID string) error
-	DeleteSubcription(subj SubscriptionSubject, channelID string) error
+	StoreSubscription(subj SubscriptionSubject, sub Subscription, channelID string) error
+	DeleteSubscription(subj SubscriptionSubject, sub SubscriptionID, channelID string) error
 }
 
 type subscriptions struct {
 	configurator.Configurator
+	mm *pluginapi.Client
 }
 
 var _ Subscriptions = (*subscriptions)(nil)
@@ -39,7 +42,7 @@ func (subs *subscriptions) GetSubscriptionsForChannel(subj SubscriptionSubject, 
 	return []*Subscription{
 		{
 			AppID:     "Hello",
-			Subject:   SubjectUserJoinedChannel,
+			Subject:   subj,
 			ChannelID: "",
 		},
 	}, nil
@@ -54,20 +57,71 @@ func (subs *subscriptions) GetSubscriptionsForApp(app string, subj SubscriptionS
 	return nil, nil
 }
 
-// StoreSubscription stores a subscription
-func (subs *subscriptions) StoreSubcription(subj SubscriptionSubject, channelID string) error {
-	// TODO Implement KVSet
-	//   check if subscription already exists
-	//   if yes, update/overwrite
-	//   if no, append new subscription to subs_channelID values
+// StoreSubscription stores a subscription for a change notification
+// TODO move this to store package or file
+func (subs *subscriptions) StoreSubscription(subj SubscriptionSubject, sub Subscription, channelID string) error {
+	key := GetSubsKVkey(subj, channelID)
+
+	// get all subscriptions for the subject
+	var savedSubs []*Subscription
+	if err := subs.mm.KV.Get(key, &savedSubs); err != nil {
+		return errors.Wrap(err, "failed to get saved subscriptions")
+	}
+
+	// check if sub exists
+	var newSubs []*Subscription
+	for _, s := range savedSubs {
+		// modify the sub to the latest request
+		if s.SubscriptionID == sub.SubscriptionID {
+			newSubs = append(newSubs, &sub)
+			continue
+		}
+		newSubs = append(newSubs, s)
+	}
+
+	// sub exists. update and save updated subs
+	_, err := subs.mm.KV.Set(key, newSubs)
+	if err != nil {
+		return errors.Wrap(err, "failed to save subscriptions")
+	}
 	return nil
 }
 
 // DeleteSubscription deletes a subscription
-func (subs *subscriptions) DeleteSubcription(subj SubscriptionSubject, channelID string) error {
-	// TODO Implement KVDelete
-	//	  check if subscription exists
-	//    if yes, delete from subs_channelID values
-	//    if no, return nil
+func (subs *subscriptions) DeleteSubscription(subj SubscriptionSubject, subID SubscriptionID, channelID string) error {
+	key := GetSubsKVkey(subj, channelID)
+
+	// get all subscriptions for the subject
+	var savedSubs []*Subscription
+	if err := subs.mm.KV.Get(key, &savedSubs); err != nil {
+		return errors.Wrap(err, "failed to get saved subscriptions")
+	}
+
+	// check if sub exists
+	var newSubs []*Subscription
+	for _, s := range savedSubs {
+		if s.SubscriptionID == subID {
+			continue
+		}
+		newSubs = append(newSubs, s)
+	}
+
+	// sub was deleted. update and save updated subs
+	// TODO check for don't need to save if sub was not deleted?
+	_, err := subs.mm.KV.Set(key, newSubs)
+	if err != nil {
+		return errors.Wrap(err, "failed to save subscriptions")
+	}
 	return nil
+}
+
+func GetSubsKVkey(subj SubscriptionSubject, channelID string) string {
+	key := SubsPrefixKey
+	switch subj {
+	case SubjectUserJoinedChannel:
+		key += "_" + string(SubjectUserJoinedChannel) + channelID
+	// case value2:
+	default:
+	}
+	return key
 }
