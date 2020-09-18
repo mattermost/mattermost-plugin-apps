c46c52bee8ed683e0d24266abc9e625730a8078a
Lev <1187448+levb@users.noreply.github.com>
2020-09-18 05:27:45 -0700

    Initial data structs for forms, autocomplete, and modals (#3)
    * copied circleci.yml from demo
    
    * First implementation
    
    - General plugin skeleton: commands, configuration
    - Skeleton Registry, Proxy, Subscription entities
    - Skeleton OnUserJoinedChannel implementation
    - Basic `Expander`
    
    * lint fix
    
    * more lint
    
    * Minor additions
    
    * Initial data structs for forms, autocomplete, and modals
    
    * refined RefreshOnChangeTo
    
    * fixed package name
    
    Co-authored-by: Lev Brouk <levbrouk@levs-imac.lan>

---
+++ server/apps/autocomplete.go
@@ -0,0 +1,38 @@
+package apps
+
+type Autocomplete struct {
+	Form
+}
+
+type autocompleteElementProps struct {
+	FlagName string `json:"flag_name"`
+
+	// Hint describes what choices may come after selecting this choice
+	Hint string `json:"label"`
+
+	RoleID string `json:"role_id"`
+}
+
+type autocompleteProps struct {
+	elementProps
+	autocompleteElementProps
+}
+
+type AutocompleteText struct {
+	autocompleteProps
+	textElementProps
+}
+
+type AutocompleteStaticSelect struct {
+	autocompleteProps
+	staticSelectElementProps
+}
+
+type AutocompleteDynamicSelect struct {
+	autocompleteProps
+	dynamicSelectElementProps
+}
+
+type AutocompleteBool autocompleteProps
+type AutocompleteUser autocompleteProps
+type AutocompleteChannel autocompleteProps
---
+++ server/apps/form.go
@@ -0,0 +1,71 @@
+package apps
+
+type Form struct {
+	// RefreshOnChangeTo indicates that changes to the listed fields must reload
+	// the form. Values of the fields with values that are not included in the
+	// refreshed form are lost. Values that no longer apply are reset.
+	RefreshOnChangeTo []string `json:"refresh_on_change_to,omitempty"`
+	RefreshURL        string
+
+	Elements []interface{} // of *XXXElement
+}
+
+type ElementType string
+
+const (
+	ElementTypeCommand       = ElementType("command")
+	ElementTypeText          = ElementType("text")
+	ElementTypeStaticSelect  = ElementType("static_select")
+	ElementTypeDynamicSelect = ElementType("dynamic_select")
+	// ElementTypeTime          = ElementType("time")
+	ElementTypeBool    = ElementType("bool")
+	ElementTypeUser    = ElementType("user")
+	ElementTypeChannel = ElementType("channel")
+)
+
+type elementProps struct {
+	Type ElementType `json:"type"`
+
+	// Name is the name of the JSON field to use when submitting
+	Name string `json:"name"`
+
+	// Description is long description, can be used in modals in addition to
+	// Label, and in Autocomplete as Help
+	Description string `json:"description,omitempty"`
+
+	IsRequired bool `json:"is_required,omitempty"`
+}
+
+type staticSelectElementProps struct {
+	Options []SelectOption `json:"options,omitempty"`
+}
+
+type dynamicSelectElementProps struct {
+	// RefreshOnChangeTo indicates that changes to the listed fields must reload
+	// the list (and reset the current value if the old one is not available).
+	RefreshOnChangeTo []string `json:"refresh_on_change_to,omitempty"`
+	RefreshURL        string   `json:"url,omitempty"`
+}
+
+type textElementProps struct {
+	Subtype   string `json:"subtype,omitempty"`
+	MinLength int    `json:"min_length,omitempty"`
+	MaxLength int    `json:"max_length,omitempty"`
+	// options - encoding, regexp, etc.
+}
+
+type SelectOption struct {
+	Label string `json:"label"`
+	Value string `json:"value"`
+}
+
+// type unmarshalledElement struct {
+// 	elementProps
+
+// 	autocompleteElementProps
+// 	modalElementProps
+
+// 	staticSelectElementProps
+// 	dynamicSelectElementProps
+// 	textElementProps
+// }
