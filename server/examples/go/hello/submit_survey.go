b5f7cdb9f19f115959b553eb50bd5e8c2bf7c6e0
Daniel Espino García <larkox@gmail.com>
2021-01-29 15:44:57 +0100

    Add update example

--- server/examples/go/hello/submit_survey.go
+++ server/examples/go/hello/submit_survey.go
@@ -1,10 +1,13 @@
 package hello
 
 import (
+	"fmt"
 	"strings"
 
 	"github.com/mattermost/mattermost-plugin-apps/server/api"
+	"github.com/mattermost/mattermost-plugin-apps/server/examples"
 	"github.com/mattermost/mattermost-plugin-apps/server/utils/md"
+	"github.com/mattermost/mattermost-server/v5/model"
 )
 
 func SubmitSurvey(c *api.Call) *api.CallResponse {
@@ -16,6 +19,16 @@ func SubmitSurvey(c *api.Call) *api.CallResponse {
 		}
 	}
 	selected := location[len(location)-1]
+	if selected == "button" {
+		bot := examples.AsBot(c.Context)
+		p := &model.Post{
+			Id:      c.Context.PostID,
+			Message: "The survey will not be sent",
+		}
+		_, err := bot.UpdatePost(c.Context.PostID, p)
+		fmt.Println(err)
+		fmt.Println(c.Context.PostID)
+	}
 	return &api.CallResponse{
 		Type:     api.CallResponseTypeOK,
 		Markdown: md.Markdownf("You answered the survey with `%s`.", selected),
