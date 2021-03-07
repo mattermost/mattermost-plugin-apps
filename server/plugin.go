61c304e2fea36d31e463c0ffa7f6ad1ff6724c45
Ben Schumacher <ben.schumacher@mattermost.com>
2021-03-07 09:13:07 +0100

    Ensure first config get passed to configurator

--- server/plugin.go
+++ server/plugin.go
@@ -77,7 +77,10 @@ func (p *Plugin) OnActivate() error {
 	awsClient := aws.NewAWSClient(accessKey, secretKey, &mm.Log)
 
 	conf := configurator.NewConfigurator(mm, awsClient, p.BuildConfig, botUserID)
-	_ = conf.RefreshConfig(&stored)
+	err = conf.RefreshConfig(&stored)
+	if err != nil {
+		return errors.Wrap(err, "failed to refresh config on startup")
+	}
 	store := store.New(mm, conf)
 	proxy := proxy.NewProxy(mm, awsClient, conf, store)
 
