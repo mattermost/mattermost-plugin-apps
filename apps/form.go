8ce298ff0e59cfec7a3ccc7e2e3b6c29b8a26102
Lev Brouk <lev@mattermost.com>
2021-03-10 12:16:05 -0800

    godoc form.go

--- apps/form.go
+++ apps/form.go
@@ -1,11 +1,31 @@
 package apps
 
+// Form defines what inputs a Call accepts, and how they can be gathered from
+// the user, in Modal and Autocomplete modes.
+//
+// For a Modal, the form defines the modal entirely, and displays it when
+// returned in response to a submit Call. Modals are dynamic in the sense that
+// they may be refreshed entirely when values of certain fields change, and may
+// contain dynamic select fields.
+//
+// For Autocomplete, a form can be bound to a sub-command. The behavior of
+// autocomplete once the subcommand is selected is designed to mirror the
+// functionality of the Modal. Some gaps and differences still remain.
+//
+// Requests for forms are calls, have expanded contexts, making it easy to
+// generate forms specific to the user, channel, etc.
 type Form struct {
+	// Title, Header, and Footer are used for Modals only.
 	Title  string `json:"title,omitempty"`
 	Header string `json:"header,omitempty"`
 	Footer string `json:"footer,omitempty"`
-	Icon   string `json:"icon,omitempty"`
 
+	// A fully-qualified URL, or a path to the form icon.
+	// TODO do we default to the App icon?
+	Icon string `json:"icon,omitempty"`
+
+	// Call is the same definition used to submit, refresh the form, and to
+	// lookup dynamic select options.
 	Call *Call `json:"call,omitempty"`
 
 	// SubmitButtons refers to a field name that must be a FieldTypeStaticSelect
@@ -14,7 +34,7 @@ type Form struct {
 	// In Modal view, the field will be rendered as a list of buttons at the
 	// bottom. Clicking one of them submits the Call, providing the button
 	// reference as the corresponding Field's value. Leaving this property
-	// blank, displays the default "OK" button.
+	// blank, displays the default "OK".
 	//
 	// In Autocomplete, it is ignored.
 	SubmitButtons string `json:"submit_buttons,omitempty"`
@@ -28,5 +48,6 @@ type Form struct {
 	// refreshed form are lost.
 	DependsOn []string `json:"depends_on,omitempty"`
 
+	// Fields is the list of fields in the form.
 	Fields []*Field `json:"fields,omitempty"`
 }
