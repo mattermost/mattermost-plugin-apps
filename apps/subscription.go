b580fb9fcc4dd879a471130680732b9110bc3dca
Lev Brouk <lev@mattermost.com>
2021-03-10 17:21:38 -0800

    wip

--- apps/subscription.go
+++ apps/subscription.go
@@ -11,24 +11,55 @@ import (
 type Subject string
 
 const (
-	SubjectUserCreated       = Subject("user_created")
-	SubjectUserJoinedChannel = Subject("user_joined_channel")
-	SubjectUserLeftChannel   = Subject("user_left_channel")
-	SubjectUserJoinedTeam    = Subject("user_joined_team")
-	SubjectUserLeftTeam      = Subject("user_left_team")
-	SubjectUserUpdated       = Subject("user_updated")
-	SubjectChannelCreated    = Subject("channel_created")
-	SubjectPostCreated       = Subject("post_created")
+	// SubjectUserCreated subscribes to UserHasBeenCreated plugin events. By
+	// default, fully expanded User object is included in the notifications.
+	// There is no other data to expand.
+	SubjectUserCreated Subject = "user_created"
+
+	// SubjectUserJoinedChannel and SubjectUserLeftChannel subscribes to
+	// respective plugin events, for the specified channel. By default
+	// notifications include ActingUserID, UserID, and ChannelID, but only
+	// ActingUser is fully expanded. Expand can be used to expand other
+	// entities.
+	SubjectUserJoinedChannel Subject = "user_joined_channel"
+	SubjectUserLeftChannel   Subject = "user_left_channel"
+
+	// SubjectUserJoinedTeam and SubjectUserLeftTeam subscribes to respective
+	// plugin events, for the specified team. By default notifications include
+	// ActingUserID, UserID, and TeamID, but only ActingUser is fully expanded.
+	// Expand can be used to expand other entities.
+	SubjectUserJoinedTeam Subject = "user_joined_team"
+	SubjectUserLeftTeam   Subject = "user_left_team"
+
+	// SubjectChannelCreated subscribes to ChannelHasBeenCreated plugin events,
+	// for the specified team. By default notifications include UserID (creator),
+	// ChannelID, and TeamID, but only Channel is fully expanded. Expand can be
+	// used to expand other entities.
+	SubjectChannelCreated Subject = "channel_created"
+
+	// SubjectPostCreated subscribes to MessageHasBeenPosted plugin events, for
+	// the specified channel. By default notifications include UserID (author), PostID,
+	// RootPostID, ChannelID, but only Post is fully expanded. Expand can be
+	// used to expand other entities.
+	SubjectPostCreated Subject = "post_created"
 )
 
+// Subscription is submitted by an app to the Subscribe API. It determines what
+// events the app would like to be notified on, and how these notifications
+// should be invoked.
 type Subscription struct {
-	AppID   AppID   `json:"app_id"`
+	// AppID is used internally by Mattermost. It does not need to be set by app
+	// developers.
+	AppID AppID `json:"app_id,omitempty"`
+
+	// Subscription subject.
 	Subject Subject `json:"subject"`
 
-	// Scope
+	// ChannelID and TeamID are the subscription scope, as applicable to the subject.
 	ChannelID string `json:"channel_id,omitempty"`
 	TeamID    string `json:"team_id,omitempty"`
 
+	// Call is the (one-way) call to make upon the event.
 	Call *Call
 }
 
--- docs/05-using-mattermost-api.md
+++ docs/05-using-mattermost-api.md
@@ -15,10 +15,19 @@ What tokens the app gets, and what access the app may have with them depends on 
   - [MM-??]()
 
 ## Apps Subscriptions API
+- godoc: [Subscription](https://pkg.go.dev/github.com/mattermost/mattermost-plugin-apps/apps#Subscription) -
+  [local](http://localhost:6060/pkg/github.com/mattermost/mattermost-plugin-apps/apps#Subscription) -
+  describes the Subscription request.
+- godoc: [Go Client](https://pkg.go.dev/github.com/mattermost/mattermost-plugin-apps/apps/mmclient/#Client.Subscribe) - [local](http://localhost:6060/pkg/github.com/mattermost/mattermost-plugin-apps/apps/mmclient/#Client.Subscribe) - The methods to post Subscribe/Unsubscribe requests.
 
+Subscribe and Unsubscribe APIs are invocable with Bot, User, or Admin tokens, however they may fail if the token lacks access to the resource. For instance, the app's Bot account needs to be invited to the channel before the app can subscribe to the events in the channel as the Bot. 
 ## Apps KV Store API
+- godoc: [Go Client](https://pkg.go.dev/github.com/mattermost/mattermost-plugin-apps/apps/mmclient/#Client.KVDelete) - [local](http://localhost:6060/pkg/github.com/mattermost/mattermost-plugin-apps/apps/mmclient/#Client.KVDelete) - The methods for KV Get/Set/Delete requests.
+
+The KV APIs require the use of the Bot Account Token, and will fail if a user token is provided.
 
 ## Mattermost REST API
+- godoc: [Go Client](https://pkg.go.dev/github.com/mattermost/mattermost-plugin-apps/apps/mmclient/#Client) - [local](http://localhost:6060/pkg/github.com/mattermost/mattermost-plugin-apps/apps/mmclient/#Client) - mmclient.Client includes the *model.Client4 Mattermost REST API client, pre-initialized with the auth token.
 
 ## Go client
 
