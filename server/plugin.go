4ce1108c82695530439e1ba6e37aa5b1b2ec02c8
Catalin Tomai <catalin.tomai@mattermost.com>
2021-04-03 08:33:09 -0700

    update following latest updates

--- server/plugin.go
+++ server/plugin.go
@@ -186,7 +186,7 @@ func (p *Plugin) UserHasLeftTeam(pluginContext *plugin.Context, tm *model.TeamMe
 }
 
 func (p *Plugin) MessageHasBeenPosted(pluginContext *plugin.Context, post *model.Post) {
-	shouldProcessMessage, err := p.Helpers.ShouldProcessMessage(post, plugin.BotID(p.api.Configurator.GetConfig().BotUserID))
+	shouldProcessMessage, err := p.Helpers.ShouldProcessMessage(post, plugin.BotID(p.conf.GetConfig().BotUserID))
 	if err != nil {
 		p.mm.Log.Error("Error while checking if the message should be processed", "err", err.Error())
 		return
