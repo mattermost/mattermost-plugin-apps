f18757cb4995423ca1f8f861b692bc26992b8629
Ben Schumacher <ben.schumacher@mattermost.com>
2021-04-12 20:20:32 +0200

    Update go.mod

--- server/http/restapi/restapitestlib.go
+++ server/http/restapi/restapitestlib.go
@@ -13,8 +13,8 @@ import (
 	"testing"
 
 	"github.com/mattermost/mattermost-server/v5/api4"
-	"github.com/mattermost/mattermost-server/v5/mlog"
 	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/shared/mlog"
 	"github.com/stretchr/testify/require"
 
 	"github.com/mattermost/mattermost-plugin-apps/apps/mmclient"
