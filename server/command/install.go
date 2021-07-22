1fa1fb78a55b5a9bb09dc6cbf9d41288c9360df0
Jason Frerich <jason.frerich@mattermost.com>
2021-07-22 08:27:48 -0500

    wrap error with better description

--- server/command/install.go
+++ server/command/install.go
@@ -74,7 +74,7 @@ func (s *service) executeInstallHTTP(params *commandParams) (*model.CommandRespo
 
 	m, err := apps.ManifestFromJSON(data)
 	if err != nil {
-		return errorOut(params, err)
+		return errorOut(params, errors.Wrap(err, "unable to read `manifest json` from "+manifestURL))
 	}
 
 	_, err = s.proxy.AddLocalManifest(params.commandArgs.UserId, m)
