26c800a64a9db1ea797e3b17a4c16d20c8acdb86
Lev Brouk <lev@mattermost.com>
2021-08-30 08:54:55 -0700

    Fix Cypress and hello

--- examples/go/hello/hello.go
+++ examples/go/hello/hello.go
@@ -22,11 +22,6 @@ var bindingsData []byte
 //go:embed send_form.json
 var formData []byte
 
-const (
-	host = "localhost"
-	port = 8080
-)
-
 func main() {
 	// Serve its own manifest as HTTP for convenience in dev. mode.
 	http.HandleFunc("/manifest.json", writeJSON(manifestData))
@@ -46,9 +41,8 @@ func main() {
 	// Serves the icon for the app.
 	http.HandleFunc("/static/icon.png", writeData("image/png", iconData))
 
-	addr := fmt.Sprintf("%v:%v", host, port)
-	fmt.Printf(`hello-world app listening at http://%s`, addr)
-	http.ListenAndServe(addr, nil)
+	fmt.Println("Listening on :8080") // matches manifest.json
+	panic(http.ListenAndServe(":8080", nil))
 }
 
 func send(w http.ResponseWriter, req *http.Request) {
