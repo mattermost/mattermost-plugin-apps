6e9073735942a5cbb940e0c0b70ace61c0922c37
Michael Kochell <6913320+mickmister@users.noreply.github.com>
2021-08-31 22:41:37 -0400

    Add tests for OnActivate and OnDeactivate

---
+++ server/plugin_test.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"testing"
+
+	pluginapi "github.com/mattermost/mattermost-plugin-api"
+	"github.com/mattermost/mattermost-plugin-apps/server/config"
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestOnActivate(t *testing.T) {
+	testAPI := &plugintest.API{}
+	p := NewPlugin(
+		config.BuildConfig{
+			Manifest:       *manifest,
+			BuildHash:      BuildHash,
+			BuildHashShort: BuildHashShort,
+			BuildDate:      BuildDate,
+		},
+	)
+
+	p.API = testAPI
+
+	testAPI.On("GetServerVersion").Return("5.30.1")
+	testAPI.On("KVGet", "mmi_botid").Return([]byte("the_bot_id"), nil)
+
+	username := "appsbot"
+	displayName := "Mattermost Apps"
+	description := "Mattermost Apps Registry and API proxy."
+	testAPI.On("PatchBot", "the_bot_id", &model.BotPatch{
+		Username:    &username,
+		DisplayName: &displayName,
+		Description: &description,
+	}).Return(nil, nil)
+
+	testAPI.On("GetBundlePath").Return("../", nil)
+
+	testAPI.On("SetProfileImage", "the_bot_id", mock.AnythingOfType("[]uint8")).Return(nil)
+
+	testAPI.On("LoadPluginConfiguration", mock.AnythingOfType("*config.StoredConfig")).Return(nil)
+
+	siteURL := "http://localhost:8065"
+	testAPI.On("GetConfig").Return(&model.Config{
+		ServiceSettings: model.ServiceSettings{
+			SiteURL: &siteURL,
+		},
+	})
+
+	testAPI.On("GetLicense").Return(&model.License{
+		Features:     &model.Features{},
+		SkuShortName: "professional",
+	})
+
+	expectLog(testAPI, "LogDebug", 9)
+	expectLog(testAPI, "LogInfo", 5)
+
+	testAPI.On("RegisterCommand", mock.AnythingOfType("*model.Command")).Return(nil)
+
+	testAPI.On("PublishWebSocketEvent", "plugin_enabled", map[string]interface{}{}, &model.WebsocketBroadcast{})
+
+	err := p.OnActivate()
+	require.NoError(t, err)
+}
+
+func TestOnDeactivate(t *testing.T) {
+	testAPI := &plugintest.API{}
+	p := NewPlugin(
+		config.BuildConfig{
+			Manifest:       *manifest,
+			BuildHash:      BuildHash,
+			BuildHashShort: BuildHashShort,
+			BuildDate:      BuildDate,
+		},
+	)
+
+	p.API = testAPI
+
+	mm := pluginapi.NewClient(p.API, p.Driver)
+	p.conf = config.NewService(mm, p.BuildConfig, "the_bot_id")
+
+	testAPI.On("PublishWebSocketEvent", "plugin_disabled", map[string]interface{}{}, &model.WebsocketBroadcast{})
+
+	err := p.OnDeactivate()
+	require.NoError(t, err)
+}
+
+func expectLog(testAPI *plugintest.API, logType string, numArgs int) {
+	args := []interface{}{}
+	for i := 0; i < numArgs; i++ {
+		args = append(args, mock.Anything)
+	}
+
+	testAPI.On(logType, args...)
+}
