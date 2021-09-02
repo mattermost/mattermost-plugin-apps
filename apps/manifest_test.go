37346e9a24f6e9d9d9ef9d6cfb65839cb97bdc28
Lev Brouk <lev@mattermost.com>
2021-09-02 08:51:55 -0700

    PR feedback: moved appid, version tests to manifest.go, renamed

--- apps/manifest_test.go
+++ apps/manifest_test.go
@@ -8,7 +8,63 @@ import (
 	"github.com/mattermost/mattermost-plugin-apps/apps"
 )
 
-func TestManifestIsValid(t *testing.T) {
+func TestValidateAppID(t *testing.T) {
+	t.Parallel()
+
+	for id, valid := range map[string]bool{
+		"":                                  false,
+		"a":                                 false,
+		"ab":                                false,
+		"abc":                               true,
+		"abcdefghijklmnopqrstuvwxyzabcdef":  true,
+		"abcdefghijklmnopqrstuvwxyzabcdefg": false,
+		"../path":                           false,
+		"/etc/passwd":                       false,
+		"com.mattermost.app-0.9":            true,
+		"CAPS-ARE-FINE":                     true,
+		"....DOTS.ALSO.......":              true,
+		"----SLASHES-ALSO----":              true,
+		"___AND_UNDERSCORES____":            true,
+	} {
+		t.Run(id, func(t *testing.T) {
+			err := apps.AppID(id).Validate()
+			if valid {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestValidateAppVersion(t *testing.T) {
+	t.Parallel()
+
+	for id, valid := range map[string]bool{
+		"":            true,
+		"v1.0.0":      true,
+		"1.0.0":       true,
+		"v1.0.0-rc1":  true,
+		"1.0.0-rc1":   true,
+		"CAPS-OK":     true,
+		".DOTS.":      true,
+		"-SLASHES-":   true,
+		"_OK_":        true,
+		"v00_00_0000": false,
+		"/":           false,
+	} {
+		t.Run(id, func(t *testing.T) {
+			err := apps.AppVersion(id).Validate()
+			if valid {
+				assert.NoError(t, err)
+			} else {
+				assert.Error(t, err)
+			}
+		})
+	}
+}
+
+func TestValidateManifest(t *testing.T) {
 	t.Parallel()
 
 	for name, test := range map[string]struct {
