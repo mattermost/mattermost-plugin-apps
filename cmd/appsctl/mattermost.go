9b4c540c6bca91c6abb49d8e202c948c13780f75
Lev Brouk <lev@mattermost.com>
2021-10-11 09:45:58 -0700

    appsctl plugin deploy

--- cmd/appsctl/mattermost.go
+++ cmd/appsctl/mattermost.go
@@ -2,12 +2,14 @@
 package main
 
 import (
+	"net/http"
 	"os"
 
 	"github.com/pkg/errors"
 
 	"github.com/mattermost/mattermost-plugin-apps/apps"
 	"github.com/mattermost/mattermost-plugin-apps/apps/appclient"
+	"github.com/mattermost/mattermost-plugin-apps/utils/httputils"
 )
 
 func getMattermostClient() (*appclient.Client, error) {
@@ -40,6 +42,7 @@ func updateMattermost(m apps.Manifest, deployType apps.DeployType, installApp bo
 	// Keep the Deploy part of the stored manifest intact, just add/update the
 	// new deploy type.
 	m.Deploy = d.UpdateDeploy(m.Deploy, deployType)
+
 	_, err = appClient.StoreListedApp(m)
 	if err != nil {
 		return errors.Wrap(err, "failed to add local manifest to Mattermost")
@@ -55,4 +58,50 @@ func updateMattermost(m apps.Manifest, deployType apps.DeployType, installApp bo
 	}
 
 	return nil
+}
+
+const maxManifestSize = 10 * 1024 * 1024 // 10Mb
+
+func installPlugin(bundlePath string) (*apps.Manifest, error) {
+	appClient, err := getMattermostClient()
+	if err != nil {
+		return nil, err
+	}
+
+	f, err := os.Open(bundlePath)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open the plugin bundle")
+	}
+	defer f.Close()
+
+	pluginManifest, _, err := appClient.UploadPluginForced(f)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to upload the plugin to Mattermost")
+	}
+
+	_, err = appClient.EnablePlugin(pluginManifest.Id)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to enable plugin on Mattermost")
+	}
+
+	manifestPath := appClient.Client4.URL + "/plugins/" + pluginManifest.Id + "/manifest.json"
+	resp, err := appClient.Client4.HTTPClient.Get(manifestPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to get the app manifest %s", manifestPath)
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.Errorf("failed to get app manifest %s: status %v", manifestPath, resp.Status)
+	}
+
+	data, err := httputils.LimitReadAll(resp.Body, maxManifestSize)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to get the app manifest")
+	}
+
+	m, err := apps.DecodeCompatibleManifest(data)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to parse the app manifest")
+	}
+	return m, nil
 }
---
+++ cmd/appsctl/plugin.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/mattermost/mattermost-plugin-apps/apps"
+)
+
+func init() {
+	rootCmd.AddCommand(pluginCmd)
+
+	// deploy
+	pluginCmd.AddCommand(pluginDeployCmd)
+	pluginDeployCmd.Flags().BoolVar(&install, "install", false, "Install the plugin as a Mattermost App")
+}
+
+var pluginCmd = &cobra.Command{
+	Use:   "plugin",
+	Short: "Deploy compatible Mattermost plugins as Apps",
+}
+
+var pluginDeployCmd = &cobra.Command{
+	Use:   "deploy",
+	Short: "Deploy a compatible Mattermost plugin as an app",
+	Args:  cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		bundlePath := args[0]
+
+		m, err := installPlugin(bundlePath)
+		if err != nil {
+			return err
+		}
+
+		if err = updateMattermost(*m, apps.DeployPlugin, install); err != nil {
+			return err
+		}
+
+		fmt.Printf("\nDeployed '%s' as plugin.\n", m.DisplayName)
+
+		if !install {
+			fmt.Printf("You can now install it in Mattermost using:\n")
+			fmt.Printf("  /apps install listed %s\n\n", m.AppID)
+		}
+		return nil
+	},
+}
