84a91874e7d3217be1b371b213dc538025769b30
Ben Schumacher <ben.schumacher@mattermost.com>
2022-02-25 13:56:58 +0100

    Migration path for previously installed apps

--- server/session/session.go
+++ server/session/session.go
@@ -86,9 +86,13 @@ func (s *service) createSession(r *incoming.Request, appID apps.AppID, userID st
 
 	session.AddProp(model.SessionPropOs, "OAuth2")
 	session.AddProp(model.SessionPropBrowser, "OAuth2")
-	session.AddProp(model.SessionPropPlatform, oAuthApp.Name)
-	session.AddProp(model.SessionPropOAuthAppID, oAuthApp.Id)
 	session.AddProp(model.SessionPropMattermostAppID, string(appID))
+	// For apps installed before https://github.com/mattermost/mattermost-plugin-apps/pull/291
+	// oAuthApp is nil.
+	if oAuthApp != nil {
+		session.AddProp(model.SessionPropPlatform, oAuthApp.Name)
+		session.AddProp(model.SessionPropOAuthAppID, oAuthApp.Id)
+	}
 
 	session, err = s.mm.Session.Create(session)
 	if err != nil {
