5c5a84759594a548bc7be3d4d8ac9fbed85fa60a
Michael Kochell <mjkochell@gmail.com>
2022-10-14 01:22:42 +0000

    fix binding types

--- apps/binding_type.go
+++ apps/binding_type.go
@@ -3,19 +3,19 @@ package apps
 type BindingType string
 
 const (
-	BindingTypeView     BindingType = "view"
-	BindingTypeSection  BindingType = "section"
-	BindingTypeList     BindingType = "list"
-	BindingTypeListItem BindingType = "list_block"
-	BindingTypeMenu     BindingType = "menu"
-	BindingTypeMenuItem BindingType = "menu_item"
-	BindingTypeMarkdown BindingType = "markdown"
-	BindingTypeDivider  BindingType = "divider"
+	BindingTypeView      BindingType = "view"
+	BindingTypeSection   BindingType = "section"
+	BindingTypeListBlock BindingType = "list_block"
+	BindingTypeListItem  BindingType = "list_item"
+	BindingTypeMenu      BindingType = "menu"
+	BindingTypeMenuItem  BindingType = "menu_item"
+	BindingTypeMarkdown  BindingType = "markdown"
+	BindingTypeDivider   BindingType = "divider"
 )
 
 func (t BindingType) IsValid() bool {
 	switch t {
-	case BindingTypeView, BindingTypeList, BindingTypeListItem, BindingTypeMarkdown, BindingTypeDivider:
+	case BindingTypeView, BindingTypeSection, BindingTypeListBlock, BindingTypeListItem, BindingTypeMenu, BindingTypeMenuItem, BindingTypeMarkdown, BindingTypeDivider:
 		return true
 	default:
 		return false
