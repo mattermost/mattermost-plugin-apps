d2eefb74e849a614eb50ce7ad793d19d3e755396
dsharma522 <50877070+dsharma522@users.noreply.github.com>
2022-11-01 00:14:00 +0530

    test fix

--- test/restapitest/helper.go
+++ test/restapitest/helper.go
@@ -236,9 +236,11 @@ func (th *Helper) requireEqualApp(level apps.ExpandLevel, asSystemAdmin bool, ex
 	}
 	require.NotNil(th, got, "expected: %+v", expected)
 
-	// Only sysadmins get the webhook secret expanded.
-	if !asSystemAdmin {
+	// Only sysadmins and expansion `all` for app get the webhook secret expanded.
+	if !asSystemAdmin || level != apps.ExpandAll {
 		expected.WebhookSecret = ""
+	} else {
+		require.NotNil(th, expected.WebhookSecret)
 	}
 	require.EqualValues(th, expected, got)
 }
