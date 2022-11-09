1beed6762fa602217c92014a61e06007bab89b45
Ben Schumacher <ben.schumacher@mattermost.com>
2022-11-09 12:42:55 +0100

    Move AppMetadataForClient to http layer

--- server/httpin/call.go
+++ server/httpin/call.go
@@ -11,6 +11,23 @@ import (
 	"github.com/mattermost/mattermost-plugin-apps/utils/httputils"
 )
 
+// CallResponse contains everything the CallResponse struct contains, plus some additional
+// data for the client, such as information about the App's bot account.
+//
+// Apps will use the CallResponse struct to respond to a CallRequest, and the proxy will
+// decorate the response using the CallResponse to provide additional information.
+type CallResponse struct {
+	apps.CallResponse
+
+	// Used to provide info about the App to client, e.g. the bot user id
+	AppMetadata AppMetadataForClient `json:"app_metadata"`
+}
+
+type AppMetadataForClient struct {
+	BotUserID   string `json:"bot_user_id,omitempty"`
+	BotUsername string `json:"bot_username,omitempty"`
+}
+
 // Call handles a call request for an App.
 //
 //	Path: /api/v1/call
@@ -34,7 +51,16 @@ func (s *Service) Call(r *incoming.Request, w http.ResponseWriter, req *http.Req
 	r = r.WithDestination(creq.Context.UserAgentContext.AppID)
 
 	// Call the app.
-	cresp := s.Proxy.InvokeCall(r, *creq)
+	app, cresp := s.Proxy.InvokeCall(r, *creq)
+	ccresp := CallResponse{
+		CallResponse: cresp,
+	}
+	if app != nil {
+		ccresp.AppMetadata = AppMetadataForClient{
+			BotUserID:   app.BotUserID,
+			BotUsername: app.BotUsername,
+		}
+	}
 
 	// <>/<> TODO move to proxy.
 	// Only track submit calls.
@@ -42,5 +68,5 @@ func (s *Service) Call(r *incoming.Request, w http.ResponseWriter, req *http.Req
 		s.Config.Telemetry().TrackCall(string(creq.Context.AppID), string(creq.Context.Location), r.ActingUserID(), "submit")
 	}
 
-	_ = httputils.WriteJSON(w, cresp)
+	_ = httputils.WriteJSON(w, ccresp)
 }
