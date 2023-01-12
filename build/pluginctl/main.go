0f1e47fb430098220c577042d5c5fe85356ca1a1
Michael Kochell <mjkochell@gmail.com>
2023-01-12 22:54:27 +0000

    Revert "force local deploy if andmin user env var not provided"
    This reverts commit 0b401b3190bc89e99d472417e3ecf45506b9b140.

--- build/pluginctl/main.go
+++ build/pluginctl/main.go
@@ -56,30 +56,24 @@ func pluginctl() error {
 }
 
 func getClient() (*model.Client4, error) {
-	adminUsername := os.Getenv("MM_ADMIN_USERNAME")
+	socketPath := os.Getenv("MM_LOCALSOCKETPATH")
+	if socketPath == "" {
+		socketPath = model.LocalModeSocketPath
+	}
 
-	var client *model.Client4
-	var connected bool
+	client, connected := getUnixClient(socketPath)
+	if connected {
+		log.Printf("Connecting using local mode over %s", socketPath)
+		return client, nil
+	}
 
-	if adminUsername == "" {
-		socketPath := os.Getenv("MM_LOCALSOCKETPATH")
-		if socketPath == "" {
-			socketPath = model.LocalModeSocketPath
-		}
-
-		client, connected = getUnixClient(socketPath)
-		if connected {
-			log.Printf("Connecting using local mode over %s", socketPath)
-			return client, nil
-		}
-
-		if os.Getenv("MM_LOCALSOCKETPATH") != "" {
-			log.Printf("No socket found at %s for local mode deployment. Attempting to authenticate with credentials.", socketPath)
-		}
+	if os.Getenv("MM_LOCALSOCKETPATH") != "" {
+		log.Printf("No socket found at %s for local mode deployment. Attempting to authenticate with credentials.", socketPath)
 	}
 
 	siteURL := os.Getenv("MM_SERVICESETTINGS_SITEURL")
 	adminToken := os.Getenv("MM_ADMIN_TOKEN")
+	adminUsername := os.Getenv("MM_ADMIN_USERNAME")
 	adminPassword := os.Getenv("MM_ADMIN_PASSWORD")
 
 	if siteURL == "" {
