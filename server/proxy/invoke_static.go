a16e5dd0b225307348ef7d840a3e785090528cc6
Ben Schumacher <ben.schumacher@mattermost.com>
2023-01-30 11:36:40 +0100

    Propagate errors from /ping

--- server/proxy/invoke_static.go
+++ server/proxy/invoke_static.go
@@ -59,17 +59,22 @@ func (p *Proxy) getStatic(r *incoming.Request, app *apps.App, path string) (io.R
 // pingApp checks if the app is accessible. Call its ping path with nothing
 // expanded, ignore 404 errors coming back and consider everything else a
 // "success".
-func (p *Proxy) pingApp(ctx context.Context, app *apps.App) (reachable bool) {
+func (p *Proxy) pingApp(ctx context.Context, app *apps.App) error {
 	ctx, cancel := context.WithTimeout(ctx, pingAppTimeout)
 	defer cancel()
 
 	up, err := p.upstreamForApp(app)
 	if err != nil {
-		return false
+		return errors.Wrap(err, "failed to get upstream for app")
 	}
 
 	_, err = upstream.Call(ctx, up, *app, apps.CallRequest{
 		Call: apps.DefaultPing,
 	})
-	return err == nil || errors.Cause(err) == utils.ErrNotFound
+
+	if err != nil && errors.Cause(err) != utils.ErrNotFound {
+		return errors.Wrapf(err, "failed to call %s endpoint", apps.DefaultPing.Path)
+	}
+
+	return nil
 }
