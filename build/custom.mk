097e3767f3078e39b4114131b950a460adbe6f3e
José Peso <trilopin@users.noreply.github.com>
2023-03-30 12:06:28 +0200

    fix rudder key secret name and remove wrong env mapping (#468)

--- build/custom.mk
+++ build/custom.mk
@@ -1,11 +1,13 @@
 # Include custome targets and environment variables here
 default: all
 
-ifndef MM_RUDDER_WRITE_KEY
-    MM_RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+# If there's no MM_RUDDER_PLUGINS_PROD, add DEV data
+RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+ifdef MM_RUDDER_PLUGINS_PROD
+	RUDDER_WRITE_KEY = $(MM_RUDDER_PLUGINS_PROD)
 endif
 
-LDFLAGS += -X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(MM_RUDDER_WRITE_KEY)"
+LDFLAGS += -X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(RUDDER_WRITE_KEY)"
 
 BUILD_DATE = $(shell date -u)
 BUILD_HASH = $(shell git rev-parse HEAD)
